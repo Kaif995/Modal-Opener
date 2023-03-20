f8047deaed950cfdd90dcc497cf831e6fc0456ca
Mohammed Kaif Shamim <kaifshamim995@gmail.com>
2023-03-21 01:50:23 +0500

    1st Commit!

---
+++ app.js
@@ -0,0 +1,21 @@
+var A;
+var B;
+function foo(){
+    A=document.getElementById("mod").style.display='block'
+}
+function footwo(){
+    A=document.getElementById("modtwo").style.display='block'
+}
+function foothree(){
+    
+    A=document.getElementById("modthree").style.display='block'
+}
+ function dlt_one(){
+    B=document.getElementById("mod").style.display=''
+}
+function dlt_two(){
+    B=document.getElementById("modtwo").style.display=''
+ }
+ function dlt_three(){
+    B=document.getElementById("modthree").style.display=''
+}
